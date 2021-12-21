a3a9e0aad97026950dfcc125c4893084a1c8666b
Thiago Rotondo Sampaio <thiagorot@gmail.com>
2021-12-21 18:54:46 -0300

    fix add api url in environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: "https://gist.githubusercontent.com/thiagotrs/93596ee8f7ea2d4165aa8878a3098604/raw/31d0301567289ae7d84a2c2511528b4811d04746/vessel_tracking.json"
 };
