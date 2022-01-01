200b2a6d44fe55183a795c4bc3e5a3845b8e6a02
Thiago Rotondo Sampaio <thiagorot@gmail.com>
2022-01-01 14:57:31 -0300

    refactor(vessels-list.component): add help function to template
    n

--- src/app/shared/components/vessels-list/vessels-list.component.ts
+++ src/app/shared/components/vessels-list/vessels-list.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { Stop, Vessel } from 'src/app/core/models/vessel.model';
+import { Status, Stop, Vessel } from 'src/app/core/models/vessel.model';
 
 @Component({
   selector: 'app-vessels-list',
@@ -11,6 +11,10 @@ export class VesselsListComponent {
   @Input() vessels: Vessel[] = [];
 
   constructor() {}
+
+  isSailing(vessel: Vessel): boolean {
+    return vessel.status === Status.SAILING;
+  }
 
   filterLastPort(stops: Stop[]): string {
     if (!stops.length) return 'none';
