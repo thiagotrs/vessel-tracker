d57cf3fa501c0366f8dcbd9408724318477fadf2
Thiago Rotondo Sampaio <thiagorot@gmail.com>
2022-01-01 14:46:30 -0300

    fix(button.component): add light option in color prop
    n

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -9,7 +9,7 @@ import {
 @Component({
   selector:
     // eslint-disable-next-line @angular-eslint/component-selector
-    'button[app-button], a[app-button], input[app-button][type=submit], input[app-button][type=button], input[app-button][type=reset], button[app-button]',
+    'button[app-button], a[app-button], input[app-button][type=submit], input[app-button][type=button], input[app-button][type=reset]',
   template: '<ng-content></ng-content>',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -21,7 +21,8 @@ export class ButtonComponent {
     | 'success'
     | 'danger'
     | 'warning'
-    | 'dark' = 'primary';
+    | 'dark'
+    | 'light' = 'primary';
 
   @Input() size?: 'large' | 'small';
 
@@ -36,6 +37,7 @@ export class ButtonComponent {
       'btn-danger': this.color === 'danger' && this.outline === false,
       'btn-warning': this.color === 'warning' && this.outline === false,
       'btn-dark': this.color === 'dark' && this.outline === false,
+      'btn-light': this.color === 'light' && this.outline === false,
       'btn-outline-primary': this.color === 'primary' && this.outline !== false,
       'btn-outline-secondary':
         this.color === 'secondary' && this.outline !== false,
@@ -43,6 +45,7 @@ export class ButtonComponent {
       'btn-outline-danger': this.color === 'danger' && this.outline !== false,
       'btn-outline-warning': this.color === 'warning' && this.outline !== false,
       'btn-outline-dark': this.color === 'dark' && this.outline !== false,
+      'btn-outline-light': this.color === 'light' && this.outline !== false,
       'btn-lg': this.size === 'large',
       'btn-sm': this.size === 'small'
     };
